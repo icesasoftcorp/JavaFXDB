2061a893f638c2873bcd9fe8b36e8130483e4232
IcesaSoft Corp <icesasoftcorp@gmail.com>
2023-02-02 20:41:00 -0500

    update code

--- src/main/java/com/project/models/Student.java
+++ src/main/java/com/project/models/Student.java
@@ -3,20 +3,27 @@ package com.project.models;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 
 import com.project.dbhandler.PostgresConnect;
 
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class Student extends Person {
 
+    // private ObservableList<Student> studentList
+    // =FXCollections.observableArrayList();
     private int level;
 
     /**
      * Generates the Staff Constructor.
+     * 
      * @param id
      * @param firstName
      * @param lastName
@@ -26,13 +33,15 @@ public class Student extends Person {
      * @param level
      */
 
-     public Student (String id, String firstName, String lastName, LocalDate birthDate, String email, String phoneNumber, int level) {
+    public Student(String id, String firstName, String lastName, LocalDate birthDate, String email, String phoneNumber,
+            int level) {
         super(id, firstName, lastName, birthDate, email, phoneNumber);
         this.level = level;
-     }
+    }
 
     /**
      * Get the Student's level as integer.
+     * 
      * @return
      */
     public int getLevel() {
@@ -41,87 +50,128 @@ public class Student extends Person {
 
     /**
      * Set the Student's level as integer.
+     * 
      * @param level
      */
     public void setLevel(int level) {
         this.level = level;
     }
-    
-    /* (non-Javadoc)
+
+    /*
+     * (non-Javadoc)
+     * 
      * @see com.project.models.Person#print()
      */
     @Override
     public void print() {
         super.print();
-        System.out.println("Level: "+this.level);
+        System.out.println("Level: " + this.level);
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see com.project.models.Person#getFullName()
      */
     @Override
-    public String getFullName() { 
+    public String getFullName() {
         return super.getFullName();
     }
 
     /**
      * It exposes the property of an object as a JAVAFX property
+     * 
      * @return
      */
-    public SimpleStringProperty idProperty(){
+    public SimpleStringProperty idProperty() {
         return new SimpleStringProperty(this.getId());
     }
 
     /**
      * It exposes the property of an object as a JAVAFX property
+     * 
      * @return
      */
-    public SimpleStringProperty firstNameProperty(){
+    public SimpleStringProperty firstNameProperty() {
         return new SimpleStringProperty(this.getFirstName());
     }
 
     /**
      * It exposes the property of an object as a JAVAFX property
+     * 
      * @return
      */
-    public SimpleStringProperty lastNameProperty(){
+    public SimpleStringProperty lastNameProperty() {
         return new SimpleStringProperty(this.getLastName());
     }
 
     /**
      * It exposes the property of an object as a JAVAFX property
+     * 
      * @return
      */
-    public SimpleStringProperty emailProperty(){
+    public SimpleStringProperty emailProperty() {
         return new SimpleStringProperty(this.getEmail());
     }
-    
+
     /**
      * It exposes the property of an object as a JAVAFX property
+     * 
      * @return
      */
-    public SimpleIntegerProperty ageProperty(){
+    public SimpleIntegerProperty ageProperty() {
         return new SimpleIntegerProperty(this.getAge());
     }
 
     /**
      * It exposes the property of an object as a JAVAFX property
+     * 
      * @return
      */
-    public SimpleIntegerProperty levelProperty(){
+    public SimpleIntegerProperty levelProperty() {
         return new SimpleIntegerProperty(this.getLevel());
+    }
+
+    /**
+     * Load the student list
+     */
+
+    public static ObservableList<Student> loadStudents() {
+        PostgresConnect pgConnect = new PostgresConnect();
+        String sql = "SELECT student_id, student_firstname, student_lastname, student_birthdate, student_email, student_phone, student_level FROM students";
+        ObservableList<Student> studentList = FXCollections.observableArrayList();
+
+        try (Connection connection = pgConnect.getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                String id = resultSet.getString("student_id");
+                String firstName = resultSet.getString("student_firstname");
+                String lastName = resultSet.getString("student_lastname");
+                LocalDate birthDate = resultSet.getDate("student_birthdate").toLocalDate();
+                String email = resultSet.getString("student_email");
+                String phone = resultSet.getString("student_phone");
+                int level = resultSet.getInt("student_level");
+                studentList.add(new Student(id, firstName, lastName, birthDate, email, phone, level));
+            }
+            return studentList;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     /**
      * Save data of the student in PostgreSQL
      */
     public void saveStudent() {
-        final String sql = "INSERT INTO students" + 
-        " (student_id, student_firstname, student_lastname, student_birthdate, student_email, student_phone, student_level) VALUES " + 
-        " (?, ?, ?, ?, ?, ?, ?);"; 
+        final String sql = "INSERT INTO students" +
+                " (student_id, student_firstname, student_lastname, student_birthdate, student_email, student_phone, student_level) VALUES "
+                +
+                " (?, ?, ?, ?, ?, ?, ?);";
         PostgresConnect pgConnect = new PostgresConnect();
         Connection connection = pgConnect.getConnection();
-        
+
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
             preparedStatement.setString(1, this.getId());
             preparedStatement.setString(2, this.getFirstName());
@@ -131,7 +181,25 @@ public class Student extends Person {
             preparedStatement.setString(6, this.getPhoneNumber());
             preparedStatement.setInt(7, this.getLevel());
             preparedStatement.executeUpdate();
-        } catch(SQLException e) {
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Let delet an student from the list and database
+     * 
+     * @param student
+     */
+    public static void deleteStudent(Student student) {
+        PostgresConnect pgConnect = new PostgresConnect();
+        Connection connection = pgConnect.getConnection();
+        String sql = "DELETE FROM students WHERE student_id = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, student.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
